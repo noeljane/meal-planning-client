8d1b1e260aef0a78ee6c02cc77c127bc66a5736b
Noel Jane Bourg <noeljanebourg@gmail.com>
2021-01-11 22:26:09 -0800

    add link to new meal form

--- src/components/meals/MealsNew.js
+++ src/components/meals/MealsNew.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 import {Link} from 'react-router-dom';
 
 class MealsNew extends Component {
@@ -9,6 +8,7 @@ class MealsNew extends Component {
       name: '',
       description: '',
       date: 0,
+      link: '',
       userId: ''
       //get user from props???
     }
@@ -25,34 +25,41 @@ class MealsNew extends Component {
     })
   }
 
-  //TODO: Add axios for creating new meal
   handleSubmit = (event) => {
     event.preventDefault();
-    const { name,description, date  } = this.state;
+    const { name,description, date, link  } = this.state;
     const userId  = this.props.user.id;
+    const token = localStorage.getItem('token')
 
     let meal = {
       name: name, 
       description: description,
       date: date,
+      link: link,
+
       userId: userId
     }
     console.log("Here's the meal")
     console.log(meal);
 
-    axios.post('http://localhost:3001/meals', {meal})
-      .then(response => {
-        console.log("success!!");
-        console.log(response);
+    fetch(`http://localhost:3001/meals`, {
+        method: 'POST',
+        headers: {
+          Authorization: `Bearer ${token}`
+        },
+        body: JSON.stringify(meal)
       })
-      .catch(error => console.log('api errors:', error))
-  
-    console.log(this.state);
-  }
+      .then(resp => resp.json())
+      .then(data => {
+        console.log("success! meal added")
+        console.log(data)
+      })
+      .catch(error  => console.log('api errors: ', error))
+    }
 
   // params.require(:meal).permit(:name, :description, :date, :link_to_recipe, :user_id)
   render() {
-    const { name, description,date } = this.state;
+    const { name, description, date, link } = this.state;
     console.log("Props for meal form:");
     console.log(this.props);
     return (
@@ -78,6 +85,13 @@ class MealsNew extends Component {
             type="date"
             name="date"
             value={date}
+            onChange= {this.handleChange}>
+          </input>
+          <input
+            placeholder="Link to Recipe"
+            type="text"
+            name="link"
+            value={link}
             onChange= {this.handleChange}>
           </input>
 
